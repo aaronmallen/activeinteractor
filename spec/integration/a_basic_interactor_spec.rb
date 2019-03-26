06590aa9a6bc81bf56f31a532a27f368b73ef5f6
Aaron Allen <aaronmallen4@gmail.com>
2019-03-26 18:53:24 -0500

    added one more unit test

--- spec/integration/a_basic_interactor_spec.rb
+++ spec/integration/a_basic_interactor_spec.rb
@@ -11,6 +11,12 @@ RSpec.describe 'A Basic Interactor' do
   describe '`#perform`' do
     subject { interactor.perform }
     it { should be_an interactor.context_class }
+
+    it 'invokes #perform on the instance' do
+      expect_any_instance_of(interactor).to receive(:perform).exactly(:once)
+      subject
+    end
+
     include_examples 'A successful context'
     include_examples 'A valid context'
   end
