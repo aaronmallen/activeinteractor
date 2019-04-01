39061e5dd562bf9ee3caff137c460b720bd71ad0
Aaron Allen <aaronmallen4@gmail.com>
2019-04-01 15:36:40 -0500

    implement each_perform callbacks on organizers

--- lib/active_interactor/organizer.rb
+++ lib/active_interactor/organizer.rb
@@ -5,9 +5,141 @@ module ActiveInteractor
   #
   # @author Aaron Allen <hello@aaronmallen.me>
   # @since 0.0.1
-  # @version 0.2
+  # @version 0.3
   class Organizer < ActiveInteractor::Base
+    define_callbacks :each_perform
+
     class << self
+      # Define a callback to call after each organized interactor's
+      #  {ActiveInteractor::Base.perform} has been invoked
+      #
+      # @example
+      #  class MyInteractor1 < ActiveInteractor::Base
+      #    before_perform :print_name
+      #
+      #    def perform
+      #      puts 'MyInteractor1'
+      #    end
+      #  end
+      #
+      #  class MyInteractor2 < ActiveInteractor::Base
+      #    before_perform :print_name
+      #
+      #    def perform
+      #      puts 'MyInteractor2'
+      #    end
+      #  end
+      #
+      #  class MyOrganizer < ActiveInteractor::Organizer
+      #    after_each_perform :print_done
+      #
+      #    organized MyInteractor1, MyInteractor2
+      #
+      #    private
+      #
+      #    def print_done
+      #      puts "done"
+      #    end
+      #  end
+      #
+      #  MyOrganizer.perform(name: 'Aaron')
+      #  "MyInteractor1"
+      #  "Done"
+      #  "MyInteractor2"
+      #  "Done"
+      #  #=> <MyInteractor::Context name='Aaron'>
+      def after_each_perform(*filters, &block)
+        set_callback(:each_perform, :after, *filters, &block)
+      end
+
+      # Define a callback to call around each organized interactor's
+      #  {ActiveInteractor::Base.perform} has been invokation
+      #
+      # @example
+      #  class MyInteractor1 < ActiveInteractor::Base
+      #    before_perform :print_name
+      #
+      #    def perform
+      #      puts 'MyInteractor1'
+      #    end
+      #  end
+      #
+      #  class MyInteractor2 < ActiveInteractor::Base
+      #    before_perform :print_name
+      #
+      #    def perform
+      #      puts 'MyInteractor2'
+      #    end
+      #  end
+      #
+      #  class MyOrganizer < ActiveInteractor::Organizer
+      #    around_each_perform :print_time
+      #
+      #    organized MyInteractor1, MyInteractor2
+      #
+      #    private
+      #
+      #    def print_time
+      #      puts Time.now.utc
+      #      yield
+      #      puts Time.now.utc
+      #    end
+      #  end
+      #
+      #  MyOrganizer.perform(name: 'Aaron')
+      #  "2019-04-01 00:00:00 UTC"
+      #  "MyInteractor1"
+      #  "2019-04-01 00:00:01 UTC"
+      #  "2019-04-01 00:00:02 UTC"
+      #  "MyInteractor2"
+      #  "2019-04-01 00:00:03 UTC"
+      #  #=> <MyInteractor::Context name='Aaron'>
+      def around_each_perform(*filters, &block)
+        set_callback(:each_perform, :around, *filters, &block)
+      end
+
+      # Define a callback to call before each organized interactor's
+      #  {ActiveInteractor::Base.perform} has been invoked
+      #
+      # @example
+      #  class MyInteractor1 < ActiveInteractor::Base
+      #    before_perform :print_name
+      #
+      #    def perform
+      #      puts 'MyInteractor1'
+      #    end
+      #  end
+      #
+      #  class MyInteractor2 < ActiveInteractor::Base
+      #    before_perform :print_name
+      #
+      #    def perform
+      #      puts 'MyInteractor2'
+      #    end
+      #  end
+      #
+      #  class MyOrganizer < ActiveInteractor::Organizer
+      #    before_each_perform :print_start
+      #
+      #    organized MyInteractor1, MyInteractor2
+      #
+      #    private
+      #
+      #    def print_start
+      #      puts "Start"
+      #    end
+      #  end
+      #
+      #  MyOrganizer.perform(name: 'Aaron')
+      #  "Start"
+      #  "MyInteractor1"
+      #  "Start"
+      #  "MyInteractor2"
+      #  #=> <MyInteractor::Context name='Aaron'>
+      def before_each_perform(*filters, &block)
+        set_callback(:each_perform, :before, *filters, &block)
+      end
+
       # Declare Interactors to be invoked as part of the
       #  organizer's invocation. These interactors are invoked in
       #  the order in which they are declared
@@ -38,9 +170,11 @@ module ActiveInteractor
     #  in favor of this default implementation.
     def perform
       self.class.organized.each do |interactor|
-        self.context = interactor.new(context)
-                                 .tap(&:skip_clean_context!)
-                                 .execute_perform!
+        run_callbacks :each_perform do
+          self.context = interactor.new(context)
+                                   .tap(&:skip_clean_context!)
+                                   .execute_perform!
+        end
       end
     end
   end
