4209273cefb5e86de15b8d997528525ced072f95
Aaron Allen <aaronmallen4@gmail.com>
2019-07-24 13:38:41 -0500

    add directory argument to install generator (#54)

--- lib/rails/generators/active_interactor/install_generator.rb
+++ lib/rails/generators/active_interactor/install_generator.rb
@@ -5,23 +5,26 @@ require_relative '../active_interactor'
 module ActiveInteractor
   module Generators
     class InstallGenerator < Base
+      desc 'Install ActiveInteractor'
+      argument :directory, type: :string, default: 'interactors', banner: 'directory'
+
       def create_initializer
-        template 'initializer.rb', Rails.root.join('config', 'initializers', 'active_interactor.rb')
+        template 'initializer.erb', Rails.root.join('config', 'initializers', 'active_interactor.rb')
       end
 
       def create_application_interactor
-        template 'application_interactor.erb', Rails.root.join('app', app_dir_name, 'application_interactor.rb')
+        template 'application_interactor.erb', Rails.root.join('app', directory, 'application_interactor.rb')
       end
 
       def create_interactor_concerns
-        create_file Rails.root.join('app', app_dir_name, 'concerns', '.keep')
+        create_file Rails.root.join('app', directory, 'concerns', '.keep')
       end
 
       def autoload_interactors
         application do
           <<~CONFIG
              # autoload interactors
-            config.autoload_paths += %w[app/#{app_dir_name}]
+            config.autoload_paths += %w[app/#{directory}]
 
           CONFIG
         end
