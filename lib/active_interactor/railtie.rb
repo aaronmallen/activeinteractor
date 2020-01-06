f96e6db5279f299745f9576a0cd0fca1d08f600a
Aaron Allen <aaronmallen4@gmail.com>
2020-01-06 12:27:22 -0600

    fixed

--- lib/active_interactor/railtie.rb
+++ lib/active_interactor/railtie.rb
@@ -6,9 +6,5 @@ require 'active_interactor/rails'
 module ActiveInteractor
   class Railtie < ::Rails::Railtie
     config.active_interactor = ActiveInteractor.config
-
-    intializer 'active_interactor.add_watchable_dirs', after: :load_config_initializers do
-      config.watchable_dirs["app/#{ActiveInteractor.config.rails.directory}"] = ['.rb']
-    end
   end
 end
