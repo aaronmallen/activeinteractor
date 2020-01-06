41c6cec936b8c314e688c9a1bb658cb495fdd51a
Aaron Allen <aaronmallen4@gmail.com>
2020-01-06 12:08:37 -0600

    small fix for initializer

--- lib/active_interactor/railtie.rb
+++ lib/active_interactor/railtie.rb
@@ -7,8 +7,10 @@ module ActiveInteractor
   class Railtie < ::Rails::Railtie
     config.active_interactor = ActiveInteractor.config
 
-    initializer 'active_interactor.autoload_paths', after: :load_config_initializers do |app|
-      app.config.autoload_paths += ["app/#{ActiveInteractor.config.rails.directory}"]
+    initializer 'active_interactor.autoload_paths', after: :load_config_initializers do
+      config.after_initialize do |app|
+        app.config.autoload_paths += ["app/#{ActiveInteractor.config.rails.directory}"]
+      end
     end
   end
 end
