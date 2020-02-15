f904a855d264a1db487211ced6b8b7f68f2ffaf6
Aaron Allen <aaronmallen4@gmail.com>
2020-02-15 14:27:23 -0600

    test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,16 +2,16 @@
 
 begin
   require 'simplecov'
-  # require 'simplecov-lcov'
+  require 'simplecov-lcov'
 
-  # SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+  SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
 
-  # SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
-  #   [
-  #     SimpleCov::Formatter::HTMLFormatter,
-  #     SimpleCov::Formatter::LcovFormatter
-  #   ]
-  # )
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::HTMLFormatter,
+      SimpleCov::Formatter::LcovFormatter
+    ]
+  )
 
   SimpleCov.start do
     # enable_coverage :branch
