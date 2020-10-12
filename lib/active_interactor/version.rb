f42292ddbd4a24926586c8a4e3ab251e5c451dfb
Aaron Allen <aaronmallen4@gmail.com>
2020-10-12 18:41:24 -0500

    #251 refactor ActiveInteractor::VERSION

--- lib/active_interactor/version.rb
+++ lib/active_interactor/version.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
 module ActiveInteractor
-  # The ActiveInteractor version
-  # @return [String] the ActiveInteractor version
-  VERSION = '1.1.0'
+  module Version
+    MAJOR = 1
+    MINOR = 1
+    PATCH = 0
+    PRE = nil
+    META = nil
+
+    def self.gem_version
+      @gem_version ||= [MAJOR, MINOR, PATCH, PRE, META].compact.join('.').freeze
+    end
+
+    def self.semver
+      @semver ||= begin
+        primary = [MAJOR, MINOR, PATCH].join('.').freeze
+        return primary if PRE.nil?
+        return "#{primary}-#{PRE}" if META.nil?
+
+        "#{primary}-#{PRE}+#{META}"
+      end
+    end
+  end
 end
