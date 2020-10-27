0f47d872ccbfbfb1c7ff435a28531d9489c63af3
Aaron Allen <aaronmallen4@gmail.com>
2020-10-27 12:06:49 -0500

    #217 ensure we capture context state BEFORE rollback occurs

--- lib/active_interactor/interactor/worker.rb
+++ lib/active_interactor/interactor/worker.rb
@@ -88,8 +88,8 @@ module ActiveInteractor
 
       def handle_error(exception)
         @context = interactor.finalize_context!
-        execute_rollback
         interactor.run_callbacks :failure
+        execute_rollback
         ActiveSupport::Notifications.instrument('interactor failure') { raise exception }
       end
 
