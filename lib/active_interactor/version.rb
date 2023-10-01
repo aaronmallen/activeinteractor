f2e50e8b0053f66218b9aaf22d1ad8cec56c4a2d
Aaron Allen <hello@aaronmallen.me>
2023-09-30 22:18:24 -0500

    generate v1.2.2 release

--- lib/active_interactor/version.rb
+++ lib/active_interactor/version.rb
@@ -16,7 +16,7 @@ module ActiveInteractor
 
     # The ActiveInterctor patch version number
     # @return [Integer] The ActiveInteractor patch version number
-    PATCH = 1
+    PATCH = 2
 
     # The ActiveInterctor pre-release version
     # @return [String | nil] The ActiveInteractor pre-release version
